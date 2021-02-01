0193775f48bf03d1c4c16884393d11285b1efba4
abdelens <74510021+abdelens@users.noreply.github.com>
2021-02-01 22:24:04 +0100

    Update ipsecollect.py

--- ipsecollect.py
+++ ipsecollect.py
@@ -2,10 +2,22 @@ import sys
 import csv
 import os
 
-mylines = []
+peers = []
+ts = []
+s1="set peer"
+s2="crypto ipsec transform-set"
 with open ('input\config.txt', 'rt') as myfile:
 		for line in myfile:
-			if "crypto isakmp key" in line:
-				mylines.append(line)
-		for element in mylines:
-			print(element, end='')
+			if s1 in line:
+				#peer=line[line.index(s1) + (len(s1)+1):]
+				res = line.split()
+				peers.append(res[2])
+		print("la liste des peer est:", peers)
+
+with open ('input\config.txt', 'rt') as myfile:
+		for line in myfile:
+			if s2 in line:
+				#peer=line[line.index(s1) + (len(s1)+1):]
+				res2 = line.split()
+				ts.append(res2[2])
+		print("la liste des peer est:", peers)
